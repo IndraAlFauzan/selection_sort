d84000f36d4bf75b91c22bd7c20f8de2b90e174a
IndraAlFauzan <39118928+IndraAlFauzan@users.noreply.github.com>
2023-03-16 00:10:50 +0700

    Add project files.

---
+++ insertion_sort/insertion_sort.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+
+using namespace std;
+
+//definidikan array dengan ukuran maksimum 20
+int a[20];
+int n;
+
+
+//fungsi untuk menginputkan ukuran array
+void read() {
+	while (true) {
+		cout << "Masukkan jumlah Element/Data array :";
+		cin >> n;
+		if (n <= 20)
+			break;
+		else
+			cout << "\nArray maksimum 20 element/data.\n" << endl;
+	}
+	cout << endl;
+	cout << "......................" << endl;
+	cout << "Masukkan element Array" << endl;
+	cout << "......................" << endl;
+
+	//user inputs for the array
+	for (int i = 0; i < n; i++) {
+		cout << "<" << (i + 1) << ">";
+		cin >> a[i];
+	}
+}
+
+void display() {
+	//display the shorted array
+	cout << "total number of data movements : " << (n - 1) << endl;
+	cout << "......................" << endl;
+	cout << "Insertion Shorted array elements" << endl;
+	cout << "......................" << endl;
+
+	for (int j = 0; j < n; j++) {
+		cout << a[j] << endl;
+	}
+}
+
+void InsertionShortArry() {
+	for (int i = 1; i < n; i++) {
+		int temp = a[i];
+		int j = i - 1;
+		while ((j >= 0) && (a[j] > temp)) {
+			a[j + 1] = a[j];
+			j--;
+		}
+		a[j + 1] = temp;
+	}
+}
+
+
+int main() {
+
+	read();
+	InsertionShortArry();
+	display();
+	system("pause");
+	return 0;
+}
